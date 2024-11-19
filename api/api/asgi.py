c019615b6ddb5589ba3c356b11979200d9c770ea
Edgardo Ortega <edgardo_1986@hotmail.es>
2024-11-19 08:06:57 -0600

    socket completo

--- api/api/asgi.py
+++ api/api/asgi.py
@@ -3,20 +3,19 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
+import chat.routing
 
-import chat.routing 
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings') 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddlewareStack(
             URLRouter(
-                chat.routing.websocket_urlpatterns  
+                chat.routing.websocket_urlpatterns
             )
         )
     ),
 })
+
 
